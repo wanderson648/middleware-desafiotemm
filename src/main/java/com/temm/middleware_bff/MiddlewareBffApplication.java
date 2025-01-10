b95ad5f910c7141f50d8a1e3cc7e5904312a17ce
wanderson648 <testepedroca@gmail.com>
2025-01-10 17:29:14 -0300

    Inicia projeto

---
+++ src/main/java/com/temm/middleware_bff/MiddlewareBffApplication.java
@@ -0,0 +1,13 @@
+package com.temm.middleware_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temm/middleware_bff/MiddlewareBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temm.middleware_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddlewareBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
